5362607c1452ee37b2c68b2f61f0b6accda70eb8
John Million <jpmillion@comcast.net>
2020-11-29 05:21:56 +0000

    required pry in enviroment

--- lib/enviroment.rb
+++ lib/enviroment.rb
@@ -1,5 +1,8 @@
 require_relative "stock_screeners/version"
 require_relative "stock_screeners/cli"
+
+require "pry"
+
 module StockScreeners
   class Error < StandardError; end
   # Your code goes here...
