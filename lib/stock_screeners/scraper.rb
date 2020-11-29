d165a6ab8e1e0e69be5d9161454967a046c3fe24
John Million <jpmillion@comcast.net>
2020-11-29 19:30:59 +0000

    update

--- lib/stock_screeners/scraper.rb
+++ lib/stock_screeners/scraper.rb
@@ -6,13 +6,14 @@ class StockScreeners::Scraper
   
   def self.scrape_screeners_page(screeners_url)
     doc = Nokogiri::HTML(open(screeners_url))
+    screen_links = doc.css("#predefined-screeners a[href]").map {|e| e['href']}
     screen_names = doc.css("#predefined-screeners a[href]").map {|e| e.text}
+    screen_links.slice(1, 9)
     screen_names.slice(1, 9)
   end
   
   def self.scrape_selected_screen_page(screen_url)
     doc = Nokogiri::HTML(open(screen_url))
-    screen_links = doc.css("#predefined-screeners a[href]").map {|e| e['href']}
     table = growth.css("#fin-scr-res-table tbody")
     table_head = growth.css("#fin-scr-res-table thead")
     table_head_data_array = table_head.css("th").map{|e| e.text}
