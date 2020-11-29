3cbbba600b4d9194178106392c6364ea7f2ec87e
John Million <jpmillion@comcast.net>
2020-11-29 17:37:48 +0000

    updated

--- lib/stock_screeners/cli.rb
+++ lib/stock_screeners/cli.rb
@@ -1,9 +1,22 @@
+require_relative '../lib/scraper.rb'
+
+
 class StockScreeners::CLI
   
   BASE_URL = "https://finance.yahoo.com/screener"
   
   def call
     puts 'Welcome, please select a stock screen.'
+    display_screens
+  end
+  
+  def create_screens
+    @screens_array = Scraper.new.scrape_screeners_page(BASE_URL)
     
+  end
+  
+  def display_screens
+    screens = @screens_array[1:9]
+    screens.each {|screen| puts screen}
   end
 end
--- lib/stock_screeners/scraper.rb
+++ lib/stock_screeners/scraper.rb
@@ -7,7 +7,7 @@ class StockScreeners::Scraper
   def self.scrape_screeners_page(screeners_url)
     doc = Nokogiri::HTML(open(screeners_url))
     screen_names = doc.css("#predefined-screeners a[href]").map {|e| e.text}
-    screen_links = doc.css("#predefined-screeners a[href]").map {|e| e['href']}
+    #screen_links = doc.css("#predefined-screeners a[href]").map {|e| e['href']}
   end
   
   def self.scrape_screen_page(screen_url)
