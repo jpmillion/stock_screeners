ac8ed89a979cb38f402c3f06f7f21d13fbffe80b
John Million <jpmillion@comcast.net>
2020-11-30 04:28:54 +0000

    updated

--- lib/stock_screeners/scraper.rb
+++ lib/stock_screeners/scraper.rb
@@ -12,12 +12,9 @@ class StockScreeners::Scraper
   
   def self.scrape_selected_screen_page(screen_url)
     doc = Nokogiri::HTML(open(screen_url))
-    table = doc.css("#fin-scr-res-table tbody")
-    table_head = doc.css("#fin-scr-res-table thead")
-    table_head_data_array = table_head.css("th").map{|e| e.text}
-    table_rows = table.css("tr")
-    quotes_array = table_rows.css("a[href]").map {|e| e['href']}
-    table_row_data_array = table_rows.map {|r| r.css("td").map {|d| d.text}}
+    table_head_data_array = doc.css("#fin-scr-res-table thead th").map {|e| e.text}
+    table_row_data_array = doc.css("#fin-scr-res-table tbody tr").map {|r| r.css("td").map {|d| d.text}}
+    quotes_array = doc.css("#fin-scr-res-table tbody tr").css("a[href]").map {|e| e['href']}
     hash = {headers: table_head_data_array, rows: table_row_data_array, quotes: quotes_array}
   end
   
