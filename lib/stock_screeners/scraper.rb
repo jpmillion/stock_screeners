a4b592e68ea6597e182dd7e6fb123bab8dc7c928
John Million <jpmillion@comcast.net>
2020-11-30 23:33:45 +0000

    update

--- lib/stock_screeners/scraper.rb
+++ lib/stock_screeners/scraper.rb
@@ -14,8 +14,6 @@ class StockScreeners::Scraper
     doc = Nokogiri::HTML(open(screen_url))
     names = doc.css("#fin-scr-res-table tbody tr").map {|r| r.css("td").map {|d| d.text}}.map {|row| row[1]}.map {|name| {name: name}}
     links = doc.css("#fin-scr-res-table tbody tr").css("a[href]").map {|e| e['href']}.map {|quote| {link: quote}}
-    #names = name.map {|name| {name: name}}
-    #links = quotes_array.map {|quote| {link: quote}}
     stock_hashes = []
     i = 1 
     while i < 10 do
