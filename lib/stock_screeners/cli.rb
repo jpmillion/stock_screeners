6091df35d48457be386abba475841df3d2fbb909
John Million <jpmillion@comcast.net>
2020-11-30 20:56:57 +0000

    update

--- lib/stock_screeners/cli.rb
+++ lib/stock_screeners/cli.rb
@@ -11,26 +11,31 @@ class StockScreeners::CLI
   
   def call
     puts 'Welcome, please select a stock screen.'
-    display_screens
-    display_selected_screen(input)
-    display_summary(input)
-    start_over?
+    get_and_display_screens
+    display_selected_screen(get_input)
+    display_summary(get_input)
+    display_screen if start_over?
   end
   
   def display_selected_screen(user_input)
     self.stocks = screens[user_input].selected_screen(BASE_URL + screens[user_input].link)
   end
   
-  def display_screens
+  def get_and_display_screens
     self.screens = StockScreeners::Screen.create_screens(SCREENER_URL)
     screens.each_with_index {|screen, i| puts "Enter #{i+1} to select #{screen.name}"}
   end
+  
+  def display_screen 
+    screens.each_with_index {|screen, i| puts "Enter #{i+1} to select #{screen.name}"}
+  end
+  
   
   def display_summary(user_input)
     self.stocks[user_input].display_summary(BASE_URL + stocks[user_input].link)
   end
   
-  def input 
+  def get_input 
     user_input = gets.strip.to_i
     until user_input.between?(1, 9) do
       puts "Invalid entry, please select again."
@@ -43,6 +48,6 @@ class StockScreeners::CLI
     puts ''
     puts "To go back to stock screens enter 'y'"
     user_input = gets.strip.downcase
-    screens.each_with_index {|screen, i| puts "Enter #{i+1} to select #{screen.name}"} if user_input == 'y'
+    true if user_input == 'y'
   end
 end
